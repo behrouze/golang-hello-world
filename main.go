ecb3a32c5df371193b205b5e3a1849e60d83fa53
Behrouze <dev@behrouze.com>
2022-03-08 15:04:31 +0100

    init & basic stuff

---
+++ main.go
@@ -0,0 +1,27 @@
+// package main est mon tout premier package GO
+//
+// Ce texte descriptif permet de compléter la description de mon tout 1er package 🛸
+package main
+
+import (
+	f "fmt"
+	t "time"
+)
+
+func main() {
+	f.Println("Hello World")
+	f.Println(t.Now())
+
+	f.Println("    `.-::::::-.`    ")
+	f.Println(".:-::::::::::::::-:.")
+	f.Println("`_:::    ::    :::_`")
+	f.Println(" .:( ^   :: ^   ):. ")
+	f.Println(" `:::   (..)   :::. ")
+	f.Println(" `:::::::UU:::::::` ")
+	f.Println(" .::::::::::::::::. ")
+	f.Println(" O::::::::::::::::O ")
+	f.Println(" -::::::::::::::::- ")
+	f.Println(" `::::::::::::::::` ")
+	f.Println("  .::::::::::::::.  ")
+	f.Println("    oO:::::::Oo     ")
+}
